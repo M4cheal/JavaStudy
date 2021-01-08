576ebdcbee31f447f1db61e5d318523d910d5d05
M4cheal <527059095@qq.com>
2021-01-08 09:28:32 +0800

    异常处理

--- src/Grade.java
+++ src/Grade.java
@@ -2,15 +2,29 @@ import java.util.Scanner;
 
 public class Grade {
     public static void main(String[] args) {
-        int a;
+        String a;
+        int b;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please input Grade:");
         try {
-            a = scanner.nextInt();
-            System.out.println((a >= 0 && a <= 100) ? Grade2Str(a) : "Please input right grade!");
-        }catch (Exception e)
+            a = scanner.next();
+            b = Integer.parseInt(a);
+            inputGrade(b);
+            if (a == "")
+            {
+                throw new InputNumException("Not Null!");
+            }
+            System.out.println(Grade2Str(b));
+            // System.out.println((b >= 0 && b <= 100) ? Grade2Str(b) : "Please input right grade!");
+        } catch (NumberFormatException e)
         {
             System.out.println("error:please input num");
+        } catch (InputNumException e)
+        {
+            System.out.println(e.getMessage());
+        } catch (Exception e)
+        {
+            System.out.println("error:" + e.toString());
         }
     }
     public static String Grade2Str(int a)
@@ -24,5 +38,16 @@ public class Grade {
             case 6:return "及格";
             default:return "不及格";
         }
+    }
+    public static void inputGrade(int b) throws InputNumException {
+        if (!(b >= 0 && b <= 100)) {
+            throw new InputNumException("Please input right grade!");
+        }
+    }
+}
+class InputNumException extends Exception{
+    public InputNumException(){}
+    public InputNumException(String msg){
+        super(msg);
     }
 }
